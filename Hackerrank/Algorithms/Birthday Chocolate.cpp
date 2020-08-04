4eb3c355ffb5e38b2bbadc5b599481cfa22d9c77
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2020-08-05 03:27:03 +0530

    Add files via upload

---
+++ Hackerrank/Algorithms/Birthday Chocolate.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i,a[1000],dt,m,temp,count=0,sum=0,j;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    cin>>dt;
+    cin>>m;
+    for(i=0;i<n;i++)
+    {
+        sum=0;
+        temp=0;
+        for(j=i;j<n;j++)
+        {
+            if(temp<m)
+            {
+                sum=sum+a[j];
+                temp++;
+            }
+        }
+        if(sum==dt)
+        {
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
